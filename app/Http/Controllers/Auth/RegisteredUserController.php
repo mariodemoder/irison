145301109450cbc45a83c7d41bd6698317388d27
Mario Ajenandro Muñoz <maralejandromunoz@gmail.com>
2026-01-11 02:16:53 +0100

    first loggin
    first loggin

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Clinic;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $lastClinicId = Clinic::max('id');
+        $nextClinicId = $lastClinicId ? ($lastClinicId + 1) : 1;
+
+        $user = User::create([
+            'clinic_id' => $nextClinicId,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password_hash' => Hash::make($request->password)
+
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(route('dashboard', absolute: false));
+    }
+}
